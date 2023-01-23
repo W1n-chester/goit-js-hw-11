cee72afa2c266de974b7a2bf28893c98ff61cf24
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-01-23 17:47:20 +0200

    что то есть

--- src/index.js
+++ src/index.js
@@ -2,44 +2,80 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import {serverRequest} from './js/function_server_request'
 
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
+const simpleLightboxOptions = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+const gallerySet = new SimpleLightbox('.gallery a', simpleLightboxOptions);
 refs.form.addEventListener('submit', submitQuery);
-refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+// refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 function submitQuery(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
+  let page = 1;
     if (!evt.currentTarget.elements.searchQuery.value) {
-      return Notiflix.Notify.failure(
+      return Notify.failure(
         "Sorry, there are no images matching your search query. Please try again."
       );
-    }
-    clearGallery();
-    refs.loadMoreBtn.classList.add('is-hidden');
+  }
+  const customRequest = evt.currentTarget.elements.searchQuery.value;
+  console.log(customRequest)
+  clearGallery();
+  serverRequest(customRequest, page)
+    .then(data => {
+      appendGalleryMurkup(data);
+      gallerySet.refresh();
+    })
+ 
+ 
 }
 function clearGallery() {
   refs.gallery.innerHTML = '';
 }
-
-function markup(params) {
-  return `<div class="photo-card">
-      <img src="" alt="" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b>
-        </p>
-        <p class="info-item">
-          <b>Views</b>
-        </p>
-        <p class="info-item">
-          <b>Comments</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads</b>
-        </p>
-      </div>
-    </div>`;
+function appendGalleryMurkup(data) {
+  refs.gallery.insertAdjacentHTML('beforeend', createMurkup(data));
+  
+  
+}
+function createMurkup(data) {
+  const dataForMurcup = data.hits;
+  return dataForMurcup
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+<div class="photo-card">
+<a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" title="" width="290px" height="190px"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b><span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b><span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b><span>${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b><span>${downloads}</span>
+    </p>
+  </div>
+</div>
+`;
+      }
+    )
+    .join('');
 }
--- src/js/function_server_request.js
+++ src/js/function_server_request.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 const KEY = `33042709-ff335eccdd4e8b99fec3c8b69`;
-export function serverRequest(params) {
-    
+const MAIN_URL = `https://pixabay.com/api/`;
+
+export async function serverRequest(customRequest, page) {
+    const fulUrl = `${MAIN_URL}?key=${KEY}&q=${customRequest}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+    const respons = await axios.get(fulUrl)
+    const data = respons.data
+    console.log(data)
+
+    return data
+   
 }
